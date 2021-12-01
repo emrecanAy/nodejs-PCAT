e3147888124c23131bda37a5fffd79eb10f12ec2
emrecanAy <codemrecan@gmail.com>
2021-12-01 15:34:46 +0300

    Static files uploaded!

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const fn = function () {
-  console.log('function working');
-};
-    
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//MIDDLEWARES
+app.use(express.static('public')); //middleware-ara yazılım(req-res döngüsü içindeki her şeye middleware denir.)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu port:${port}'de başlatıldı!`);
+});
