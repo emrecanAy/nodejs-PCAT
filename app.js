c50b382e33f8b42cd2b1861ff4398c3e9ede943e
emrecanAy <codemrecan@gmail.com>
2021-12-02 17:49:17 +0300

    before MVC redesign

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 
 const Photo = require('./models/Photo');
+const { RSA_NO_PADDING } = require('constants');
 
 const app = express();
 
@@ -21,10 +25,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); //middleware-ara yazılım(req-res döngüsü içindeki her şeye middleware denir.)
 app.use(express.urlencoded({ extended: true })); //express.urlencoded ve express.urlencoded; bunlar req-res döngüsünde aldığımız req'i sonlandırmamızda yardımcı oldu.
 app.use(express.json()); //express.urlencoded = url'deki datayı oku. express.json = url'deki datayı jsona çevir.
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -47,8 +57,46 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+});
+
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body); //calback func create olana kadar bekleyecek.
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
   res.redirect('/');
 });
 
