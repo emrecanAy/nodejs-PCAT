e0d2bd5d46228bf7d7e430d09ef78b75b6cc5883
emrecanAy <codemrecan@gmail.com>
2021-12-03 23:06:15 +0300

    pagination added!

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -3,10 +3,26 @@ const fs = require('fs');
 const Photo = require('../models/Photo');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+
+  const totalPhotos = await Photo.find().countDocuments();
+
+  const photos = await Photo.find({})
+    .sort('-dateCreated')
+    .skip((page - 1) * photosPerPage)
+    .limit(photosPerPage);
+
   res.render('index', {
-    photos,
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage),
   });
+  // console.log(req.query);
+  // const photos = await Photo.find({}).sort('-dateCreated');
+  // res.render('index', {
+  //   photos,
+  // });
 };
 
 exports.getPhoto = async (req, res) => {
